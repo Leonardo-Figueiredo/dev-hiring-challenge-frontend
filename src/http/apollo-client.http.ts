828f7e9815e46aeeba3febf85eeba236c2de08bb
Leonardo Figueiredo <leo.nardorf22@gmail.com>
2022-06-25 08:35:44 -0300

    refactor(apollo-client.http): remove __typename on gql objects

--- src/http/apollo-client.http.ts
+++ src/http/apollo-client.http.ts
@@ -2,5 +2,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 export const apolloClient = new ApolloClient({
   uri: process.env.NEXT_PUBLIC_API_URL,
-  cache: new InMemoryCache()
+  cache: new InMemoryCache({
+    addTypename: false
+  })
 })
