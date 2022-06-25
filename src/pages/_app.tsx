9a57589069e700e9c3ce1a1ddeae346cabc41e27
Leonardo Figueiredo <leo.nardorf22@gmail.com>
2022-06-25 08:37:03 -0300

    feat(theme): add roboto font and set default on theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,12 @@ import { ApolloProvider } from '@apollo/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 import { apolloClient } from '../http/apollo-client.http'
+import { theme } from '../styles/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={apolloClient}>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <Component {...pageProps} />
       </ChakraProvider>
     </ApolloProvider>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,15 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+
         <body>
           <ColorModeScript />
           <Main />
