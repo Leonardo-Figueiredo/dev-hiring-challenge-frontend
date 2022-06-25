160f28695c47fd8c1d8a03e820aea53944de0b20
Leonardo Figueiredo <leo.nardorf22@gmail.com>
2022-06-25 11:12:15 -0300

    feat(repofindone.query): add Repo Find One GQL query

--- src/graphql/queries/github-repository/repoFindOne.query.ts
+++ src/graphql/queries/github-repository/repoFindOne.query.ts
@@ -1,16 +1,17 @@
 import { gql } from '@apollo/client'
 import { Repo } from '../../../entities/repo.entity'
 
-export interface CreateRepoInput
-  extends Omit<Repo, 'db_id' | 'is_storaged' | 'created_at' | 'updated_at' | 'deleted_at'> {}
-
-export interface CreateRepo {
-  createRepoInput: CreateRepoInput
+export interface FindOneRepoInput {
+  repository_full_name: string
 }
 
-export const CREATE_REPO = gql`
-  mutation createRepo($createRepoInput: CreateRepoInput!) {
-    createRepo(createRepoInput: $createRepoInput) {
+export interface FindOneRepo {
+  repoFindOne: Repo
+}
+
+export const REPO_FIND_ONE = gql`
+  query repoFindOne($repository_full_name: String!) {
+    repoFindOne(repository_full_name: $repository_full_name) {
       db_id
       id
       name
