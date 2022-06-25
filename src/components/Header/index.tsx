c220787a15d0945e057b48610938516dc2603371
Leonardo Figueiredo <leo.nardorf22@gmail.com>
2022-06-24 22:24:59 -0300

    feat(header): add header component style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,22 @@
-import { Flex, Text } from '@chakra-ui/react'
+import { Center, Img, Text } from '@chakra-ui/react'
+const ateliwareLogo = '/assets/logo_ateliware.svg'
 
 export function Header() {
   return (
-    <Flex mt={['10', '20', '20', '40']}>
+    <Center mt={['10', '20', '20', '40']} width="100%">
+      <Center
+        rounded="full"
+        border="solid 2px #222"
+        bgColor="#222"
+        p="2"
+        mx={['5']}
+      >
+        <Img src={ateliwareLogo} width={['30px']} />
+      </Center>
+
       <Text fontSize={['2xl', '2xl', '3xl']}>
         Ateliware Dev Hiring Challenge
       </Text>
-    </Flex>
+    </Center>
   )
 }
