09cd6a8ce6337e9aea2b1a6c3e65091acac13b58
Leonardo Figueiredo <leo.nardorf22@gmail.com>
2022-06-24 22:44:02 -0300

    feat(repoFindAll.query): add repoFindAll query

---
+++ src/graphql/queries/github-repository/repoFindAll.query.ts
@@ -0,0 +1,33 @@
+import { gql } from '@apollo/client'
+import { RepoCollection } from '../../../entities/repo-collection.entity'
+
+export interface RepoFindAll {
+  repoFindAll: RepoCollection[]
+}
+
+export const REPO_FIND_ALL = gql`
+  query repoFindAll {
+    repoFindAll {
+      language
+      repositories {
+        db_id
+        id
+        name
+        full_name
+        description
+        html_url
+        url
+        stargazers_count
+        watchers_count
+        language
+        open_issues
+        forks
+        watchers
+        is_storaged
+        created_at
+        updated_at
+        deleted_at
+      }
+    }
+  }
+`
