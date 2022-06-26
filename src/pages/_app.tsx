ceeeadd6693bfaa6fe1c2209d7cd5db93a8524e4
Leonardo Figueiredo <leo.nardorf22@gmail.com>
2022-06-26 12:56:38 -0300

    feat(_app): add Ateliware favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,23 @@
 import { ApolloProvider } from '@apollo/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { apolloClient } from '../http/apollo-client.http'
 import { theme } from '../styles/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ApolloProvider client={apolloClient}>
-      <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </ApolloProvider>
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/assets/logo_ateliware.svg" />
+      </Head>
+
+      <ApolloProvider client={apolloClient}>
+        <ChakraProvider theme={theme}>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </ApolloProvider>
+    </>
   )
 }
 
